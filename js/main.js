d086a3ad107196f799a8c25af3bb7a9a424c9120
dima4567254 <dima.xipkov@gmail.com>
2023-01-19 00:48:54 +0200

    19.01.2023.1

--- js/main.js
+++ js/main.js
@@ -1,15 +1,53 @@
 $(function name(params) {
+// что бы текст выпадал
+    $('.offer__item--1').on('click', function () {
+        $('.offer__items--1').slideToggle();
+    });
+    $('.offer__item--2').on('click', function () {
+        $('.offer__items--2').slideToggle();
+    });
+    $('.offer__item--3 ').on('click', function () {
+        $('.offer__items--3').slideToggle();
+    });
 
+    // let showModal = document.querySelector('#item');
+    // let hideModal = document.querySelector('#modal-hide');
+    // // при клике
+    // function show() {
+    //     // document.querySelector('.offer__item').onclick;
+    //     // modal.style.display = 'block';
+    //     // console.log('');
+    //     let modal = document.querySelector('#show');
+    //     // modal.hidden = true;
+    //     modal.style.display = 'block';
+    // }
+    // function hide() {
+    //     // document.querySelector('.offer__item').onclick;
+    //     // modal.style.display = 'block';
+    //     // console.log('');
+    //     let modal = document.querySelector('#show');
+    //     // modal.hidden = true;
+    //     modal.style.display = 'none';
+    // }
+    // showModal.onclick = show;
+    // hideModal.onclick = hide;
+    // hide()
+    // console.log('');
+
+    // $('.offer__item').on('click', function () {
+    //     $('.offer__items').removeClass('offer__items--active');
+    //     $(this).addClass('offer__items--active');
+    // });
 
 
     /*	$(".menu a").on("click", function (event) {
-    	    event.preventDefault();
-    	    var id = $(this).attr('href'),
-    	        top = $(id).offset().top;
-    	    $('body,html').animate({
-    	        scrollTop: top
-    	    }, 1500);
-    	});
+            event.preventDefault();
+            var id = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({
+                scrollTop: top
+            }, 1500);
+        });
 
         /*для закрыть меню*/
     /*
