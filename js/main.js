7abb454cf916571136b3fd900393e04e723cda68
dima4567254 <dima.xipkov@gmail.com>
2023-02-25 01:15:13 +0200

    25.02.2023.1

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
 $(function name(params) {
-    // что бы текст выпадал
-    $('.offer__item--1').on('click', function () {
-        $('.offer__items--1').slideToggle();
-    });
-    $('.offer__item--2').on('click', function () {
-        $('.offer__items--2').slideToggle();
-    });
-    $('.offer__item--3 ').on('click', function () {
-        $('.offer__items--3').slideToggle();
-    });
-
     var swiper = new Swiper(".mySwiper", {
         pagination: {
             slidesPerView: 4,
@@ -22,6 +11,106 @@ $(function name(params) {
             delay: 4000,
         },
     });
+
+    $(".menu a").on("click", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+
+    $('.menu__btn, .menu a').on('click', function () {
+        $('.menu__btn, .menu__mobile').toggleClass('menu__mobile--active');
+        // для крестика и для меню и что бы закрывалось меню после выбора сcылки
+    });
+
+    $('.offer__item--1').on('click', function () {
+        $('.offer__items--1').slideToggle();
+    });
+    $('.offer__item--2').on('click', function () {
+        $('.offer__items--2').slideToggle();
+    });
+    $('.offer__item--3 ').on('click', function () {
+        $('.offer__items--3').slideToggle();
+    });
+    // if (window.innerWidth >= 940) {
+    //     show()
+    // }
+    // alert("g");
+    // alert("Hello");
+
+    // document.addEventListener("DOMContentLoaded", function (event) {
+    //     /*ПОЛУЧАЕТ ТЕКУЩУЮ ШИРИНУ ЭКРАНА*/
+    //     console.log('8');
+    //     var widthWind = document.querySelector('body').offsetWidth;
+    //     if (widthWind <= 750) {
+
+
+
+    //     }
+    // });
+
+    // function show() {
+    //     $('.offer__item--1').on('click', function () {
+    //         $('.offer__items--1').slideToggle();
+    //     });
+    //     $('.offer__item--2').on('click', function () {
+    //         $('.offer__items--2').slideToggle();
+    //     });
+    //     $('.offer__item--3 ').on('click', function () {
+    //         $('.offer__items--3').slideToggle();
+    //     });
+    // }
+
+    // $(window).resize(function () {
+    //     /*ПОЛУЧАЕТ ТЕКУЩУЮ ШИРИНУ ЭКРАНА*/
+    //     var widthWind = $(this).width();
+
+    //     if (widthWind <= 750) {
+    //         alert(ssssss);
+    //         $('.offer__item--1').on('click', function () {
+    //             $('.offer__items--1').slideToggle();
+    //         });
+    //         $('.offer__item--2').on('click', function () {
+    //             $('.offer__items--2').slideToggle();
+    //         });
+    //         $('.offer__item--3 ').on('click', function () {
+    //             $('.offer__items--3').slideToggle();
+    //         });
+    //     }
+    // }
+    // );
+    alert(ssssss);
+
+
+    // $(document).ready(function(){
+    //     $(window).on('resize', function() {
+
+    //         if($(window).width()>940) {
+
+    //             $('.offer__item--1').on('click', function () {
+    //                 $('.offer__items--1').slideToggle();
+    //             });
+    //             $('.offer__item--2').on('click', function () {
+    //                 $('.offer__items--2').slideToggle();
+    //             });
+    //             $('.offer__item--3 ').on('click', function () {
+    //                 $('.offer__items--3').slideToggle();
+    //             });
+
+    //         }
+    //         // if($(window).width()<940){
+
+    //         //     $('.accordion-body').removeClass('in').removeAttr('style');
+
+    //         // }
+
+    //     });
+    //     $(window).resize();
+    //  });
+
+
+});
 
 
     // var swiper = new Scwiper(".myswiper", {
@@ -138,4 +227,3 @@ $(function name(params) {
            */
 
 
-});
